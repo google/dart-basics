24e6badd44607f0ef9050e56dbe0d49bd40a713a
Tianguang Zhang <penn.zht@gmail.com>
2020-11-17 12:46:56 +0100

    Remove comments.

--- test/object_basics_test.dart
+++ test/object_basics_test.dart
@@ -27,8 +27,6 @@ void main() {
       List? object5;
       Map? object7;
       Set? object9;
-      // The following line should work if written as `var object10;`,
-      // but currently it raises an error.
       Object? object10;
 
       expect(object1.isNull, isTrue);
@@ -83,8 +81,6 @@ void main() {
       List? object5;
       Map? object7;
       Set? object9;
-      // The following line should work if written as `var object10;`,
-      // but currently it raises an error.
       Object? object10;
 
       expect(object1.isNotNull, isFalse);
