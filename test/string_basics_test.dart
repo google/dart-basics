49bf79a9c3fdf4169297f3477a813218e65e293f
unknown <magbanua.c.ariel@gmail.com>
2023-02-23 01:43:00 +0800

    Fixed typo.

--- test/string_basics_test.dart
+++ test/string_basics_test.dart
@@ -105,7 +105,7 @@ void main() {
 
     test(
         'treats whole string as occurring before pattern if pattern is not '
-        'found', () {
+            'found', () {
       final string = 'word';
       expect(string.partition('z'), ['word', '', '']);
     });
@@ -146,7 +146,7 @@ void main() {
 
     test(
         'accepts steps that do not evenly divide the total number of '
-        'characters', () {
+            'characters', () {
       final string = 'word';
       expect(string.slice(step: 3), 'wd');
     });
@@ -168,7 +168,7 @@ void main() {
 
     test(
         'starts from the final character if start is greater than the length '
-        'of the string', () {
+            'of the string', () {
       final string = 'word';
       expect(string.slice(start: 100, end: 1), '');
       expect(string.slice(start: 100, end: 1, step: -1), 'dr');
@@ -176,21 +176,21 @@ void main() {
 
     test(
         'ends with the final character if end is greater than the length of '
-        'the string', () {
+            'the string', () {
       final string = 'stuff';
       expect(string.slice(start: 2, end: 100), 'uff');
     });
 
     test(
         'starts from the first character if start is less than the negative '
-        'length of the string', () {
+            'length of the string', () {
       final string = 'hat';
       expect(string.slice(start: -100, end: 3), 'hat');
     });
 
     test(
         'ends with the first character if end is less than the negative length '
-        'of the string', () {
+            'of the string', () {
       final string = 'word';
       expect(string.slice(start: 2, end: -100), '');
       expect(string.slice(start: 2, end: -100, step: -1), 'row');
@@ -204,7 +204,7 @@ void main() {
 
     test(
         'returns an empty string if start and end are not equal but correspond '
-        'to the same index', () {
+            'to the same index', () {
       final string = 'word';
       expect(string.slice(start: 1, end: -3), '');
       expect(string.slice(start: 2, end: -2), '');
@@ -212,7 +212,7 @@ void main() {
 
     test(
         'returns an empty string if start (or its equivalent index) is greater '
-        'than end (or its equivalent index) and step is positive', () {
+            'than end (or its equivalent index) and step is positive', () {
       final string = 'word';
       expect(string.slice(start: 3, end: 1), '');
       expect(string.slice(start: -1, end: 1), '');
@@ -223,7 +223,7 @@ void main() {
 
     test(
         'returns an empty string if start (or its equivalent index) is less '
-        'than end (or its equivalent index) and step is negative', () {
+            'than end (or its equivalent index) and step is negative', () {
       final string = 'word';
       expect(string.slice(start: 1, end: 3, step: -1), '');
       expect(string.slice(start: -3, end: 3, step: -1), '');
@@ -233,63 +233,63 @@ void main() {
     });
 
     test('behaves predictably when the bounds are increased in any direction',
-        () {
-      final string = 'word';
+            () {
+          final string = 'word';
 
-      expect(string.slice(start: 0, end: 0), '');
-      expect(string.slice(start: 0, end: 1), 'w');
-      expect(string.slice(start: 0, end: 2), 'wo');
-      expect(string.slice(start: 0, end: 3), 'wor');
-      expect(string.slice(start: 0, end: 4), 'word');
-      expect(string.slice(start: 0, end: 5), 'word');
+          expect(string.slice(start: 0, end: 0), '');
+          expect(string.slice(start: 0, end: 1), 'w');
+          expect(string.slice(start: 0, end: 2), 'wo');
+          expect(string.slice(start: 0, end: 3), 'wor');
+          expect(string.slice(start: 0, end: 4), 'word');
+          expect(string.slice(start: 0, end: 5), 'word');
 
-      expect(string.slice(start: 0, end: -5), '');
-      expect(string.slice(start: 0, end: -4), '');
-      expect(string.slice(start: 0, end: -3), 'w');
-      expect(string.slice(start: 0, end: -2), 'wo');
-      expect(string.slice(start: 0, end: -1), 'wor');
+          expect(string.slice(start: 0, end: -5), '');
+          expect(string.slice(start: 0, end: -4), '');
+          expect(string.slice(start: 0, end: -3), 'w');
+          expect(string.slice(start: 0, end: -2), 'wo');
+          expect(string.slice(start: 0, end: -1), 'wor');
 
-      expect(string.slice(start: 5, end: 4), '');
-      expect(string.slice(start: 4, end: 4), '');
-      expect(string.slice(start: 3, end: 4), 'd');
-      expect(string.slice(start: 2, end: 4), 'rd');
-      expect(string.slice(start: 1, end: 4), 'ord');
+          expect(string.slice(start: 5, end: 4), '');
+          expect(string.slice(start: 4, end: 4), '');
+          expect(string.slice(start: 3, end: 4), 'd');
+          expect(string.slice(start: 2, end: 4), 'rd');
+          expect(string.slice(start: 1, end: 4), 'ord');
 
-      expect(string.slice(start: -1, end: 4), 'd');
-      expect(string.slice(start: -2, end: 4), 'rd');
-      expect(string.slice(start: -3, end: 4), 'ord');
-      expect(string.slice(start: -4, end: 4), 'word');
-      expect(string.slice(start: -5, end: 4), 'word');
+          expect(string.slice(start: -1, end: 4), 'd');
+          expect(string.slice(start: -2, end: 4), 'rd');
+          expect(string.slice(start: -3, end: 4), 'ord');
+          expect(string.slice(start: -4, end: 4), 'word');
+          expect(string.slice(start: -5, end: 4), 'word');
 
-      expect(string.slice(start: 3, end: 3, step: -1), '');
-      expect(string.slice(start: 3, end: 2, step: -1), 'd');
-      expect(string.slice(start: 3, end: 1, step: -1), 'dr');
-      expect(string.slice(start: 3, end: 0, step: -1), 'dro');
+          expect(string.slice(start: 3, end: 3, step: -1), '');
+          expect(string.slice(start: 3, end: 2, step: -1), 'd');
+          expect(string.slice(start: 3, end: 1, step: -1), 'dr');
+          expect(string.slice(start: 3, end: 0, step: -1), 'dro');
 
-      expect(string.slice(start: 3, end: -1, step: -1), '');
-      expect(string.slice(start: 3, end: -2, step: -1), 'd');
-      expect(string.slice(start: 3, end: -3, step: -1), 'dr');
-      expect(string.slice(start: 3, end: -4, step: -1), 'dro');
-      expect(string.slice(start: 3, end: -5, step: -1), 'drow');
+          expect(string.slice(start: 3, end: -1, step: -1), '');
+          expect(string.slice(start: 3, end: -2, step: -1), 'd');
+          expect(string.slice(start: 3, end: -3, step: -1), 'dr');
+          expect(string.slice(start: 3, end: -4, step: -1), 'dro');
+          expect(string.slice(start: 3, end: -5, step: -1), 'drow');
 
-      expect(string.slice(start: 0, end: 0, step: -1), '');
-      expect(string.slice(start: 1, end: 0, step: -1), 'o');
-      expect(string.slice(start: 2, end: 0, step: -1), 'ro');
-      expect(string.slice(start: 3, end: 0, step: -1), 'dro');
-      expect(string.slice(start: 4, end: 0, step: -1), 'dro');
-      expect(string.slice(start: 5, end: 0, step: -1), 'dro');
+          expect(string.slice(start: 0, end: 0, step: -1), '');
+          expect(string.slice(start: 1, end: 0, step: -1), 'o');
+          expect(string.slice(start: 2, end: 0, step: -1), 'ro');
+          expect(string.slice(start: 3, end: 0, step: -1), 'dro');
+          expect(string.slice(start: 4, end: 0, step: -1), 'dro');
+          expect(string.slice(start: 5, end: 0, step: -1), 'dro');
 
-      expect(string.slice(start: -1, end: 0, step: -1), 'dro');
-      expect(string.slice(start: -2, end: 0, step: -1), 'ro');
-      expect(string.slice(start: -3, end: 0, step: -1), 'o');
-      expect(string.slice(start: -4, end: 0, step: -1), '');
+          expect(string.slice(start: -1, end: 0, step: -1), 'dro');
+          expect(string.slice(start: -2, end: 0, step: -1), 'ro');
+          expect(string.slice(start: -3, end: 0, step: -1), 'o');
+          expect(string.slice(start: -4, end: 0, step: -1), '');
 
-      expect(string.slice(start: -1, end: -5, step: -1), 'drow');
-      expect(string.slice(start: -2, end: -5, step: -1), 'row');
-      expect(string.slice(start: -3, end: -5, step: -1), 'ow');
-      expect(string.slice(start: -4, end: -5, step: -1), 'w');
-      expect(string.slice(start: -5, end: -5, step: -1), '');
-    });
+          expect(string.slice(start: -1, end: -5, step: -1), 'drow');
+          expect(string.slice(start: -2, end: -5, step: -1), 'row');
+          expect(string.slice(start: -3, end: -5, step: -1), 'ow');
+          expect(string.slice(start: -4, end: -5, step: -1), 'w');
+          expect(string.slice(start: -5, end: -5, step: -1), '');
+        });
   });
 
   group('reverse', () {
@@ -356,21 +356,21 @@ void main() {
   group('truncate', () {
     test(
         'returns a truncated string that has the length'
-        'based on the limit provided', () {
+            'based on the limit provided', () {
       final sentence = 'The quick brown fox jumps over the lazy dog';
       expect(sentence.truncate(20), 'The quick brown fox');
     });
 
     test(
         'returns the same string if the length of the string'
-        'is less than provided limit', () {
+            'is less than provided limit', () {
       final sentence = 'The quick brown fox';
       expect(sentence.truncate(20), 'The quick brown fox');
     });
 
     test(
         'returns a truncated string that has the length based on the length'
-        'provided without trimming the spaces at the end', () {
+            'provided without trimming the spaces at the end', () {
       final sentence = 'The quick brown fox jumps over the lazy dog';
       expect(sentence.truncate(20, trimTrailingWhitespace: false),
           'The quick brown fox ');
@@ -378,7 +378,7 @@ void main() {
 
     test(
         'returns a truncated string that has the length based on the length'
-        'provided with a custom substitution string', () {
+            'provided with a custom substitution string', () {
       final sentence = 'The quick brown fox jumps over the lazy dog';
       expect(sentence.truncate(20, substitution: ' (...)'),
           'The quick brown fox (...)');
@@ -388,22 +388,22 @@ void main() {
 
     test(
         'returns a truncated string that has the length based on the length'
-        'provided with a custom ending string but the substitution length will be included',
-        () {
-      final sentence = 'The quick brown fox jumps over the lazy dog';
-      expect(
-        sentence.truncate(
-          12,
-          substitution: '...',
-          includeSubstitutionInLength: true,
-        ),
-        'The quick...',
-      );
-    });
+            'provided with a custom ending string but the substitution length will be included',
+            () {
+          final sentence = 'The quick brown fox jumps over the lazy dog';
+          expect(
+            sentence.truncate(
+              12,
+              substitution: '...',
+              includeSubstitutionInLength: true,
+            ),
+            'The quick...',
+          );
+        });
 
     test(
         'returns a truncated string with emojis that has the length'
-        'based on the length provided', () {
+            'based on the length provided', () {
       final sentence = 'The quick brown 🦊🦊🦊 jumps over the lazy 🐶🐶🐶';
 
       expect(
